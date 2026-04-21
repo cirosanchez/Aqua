43f17d020b1b1080bdcfa326d2d552e7c60b998f
ciro.sanchez <ciro.sanchez@ucalgary.ca>
2026-04-20 20:31:42 -0600

    fix(): Added dependencies for MongoDB and unit tests.

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,10 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+    implementation(platform("org.mongodb:mongodb-driver-bom:5.6.4"))
+    implementation("org.mongodb:mongodb-driver-kotlin-sync")
+
+    implementation("org.mongodb:bson-kotlinx")
 }
 
 kotlin {
