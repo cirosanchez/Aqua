94eca7754ff2cd55963367fb7914950a55e88341
ciro.sanchez <ciro.sanchez@ucalgary.ca>
2026-04-20 20:32:44 -0600

    test(): Single unit test for AquaConnectionManager for further testing.

---
+++ src/test/kotlin/AquaConnectionManagerTest.kt
@@ -0,0 +1,36 @@
+import dev.cirosanchez.dev.cirosanchez.aqua.connection.AquaConnectionManager
+import org.bson.Document
+import kotlin.test.Test
+import kotlin.test.assertNotNull
+
+class AquaConnectionManagerTest {
+
+    @Test
+    fun testConnectionAndInsert() {
+
+        val connection =
+            AquaConnectionManager(
+                "mongodb://localhost:27017",
+                "aquatest"
+            )
+
+        val collection =
+            connection.getCollection("players")
+
+        val doc =
+            Document()
+                .append("_id", "junit-test")
+                .append("coins", 200)
+
+        collection.insertOne(doc)
+
+        val found =
+            collection
+                .find(Document("_id", "junit-test"))
+                .first()
+
+        assertNotNull(found)
+
+        connection.close()
+    }
+}
