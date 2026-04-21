1b91db16cd88f31408e28fb356f3ef2ad9c3922d
ciro.sanchez <ciro.sanchez@ucalgary.ca>
2026-04-20 19:45:02 -0600

    init(): Initial Commit

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    kotlin("jvm") version "2.3.10"
+}
+
+group = "dev.cirosanchez"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+kotlin {
+    jvmToolchain(11)
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
