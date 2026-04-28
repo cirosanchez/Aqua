73189796f0df7430a9a4a9628acf943e63670e01
cirosanchez <ciroandressanchezbuitrago@gmail.com>
2026-04-28 14:35:26 -0400

    feat(): Added Maven Central Repository publishing successfully.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,10 @@
 plugins {
     kotlin("jvm") version "2.3.10"
+    id("com.vanniktech.maven.publish") version "0.33.0"
 }
 
-group = "dev.cirosanchez"
-version = "1.0-SNAPSHOT"
+group = "dev.cirosanchez.aqua"
+version = "0.0.1"
 
 repositories {
     mavenCentral()
@@ -23,9 +24,38 @@ dependencies {
 }
 
 kotlin {
-    jvmToolchain(11)
+    jvmToolchain(17)
 }
 
 tasks.test {
     useJUnitPlatform()
 }
+
+mavenPublishing {
+    coordinates("dev.cirosanchez.aqua", "aqua", "0.0.1")
+
+    pom {
+        name.set("Aqua")
+        description.set("In-memory persistent cache backed by asynchronous MongoDB")
+        inceptionYear.set("2026")
+        url.set("https://github.com/cirosanchez/Aqua/")
+        licenses {
+            license {
+                name.set("MIT")
+            }
+        }
+        developers {
+            developer {
+                id.set("cirosanchez")
+                name.set("Ciro Sánchez")
+                url.set("https://github.com/cirosanchez/")
+            }
+        }
+        scm {
+            url.set("https://github.com/cirosanchez/Aqua/")
+            connection.set("scm:git:git://github.com/cirosanchez/Aqua.git")
+            developerConnection.set("scm:git:ssh://git@github.com/cirosanchez/Aqua.git")
+        }
+    }
+}
+
